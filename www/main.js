b888656f5fa5ee5043092737bd549bd270536d9f
BlueKraken <gonzalo.tajmuch@gmail.com>
2018-08-30 01:44:01 -0300

    first commit

---
+++ www/main.js
@@ -0,0 +1,5 @@
+"use strict";
+
+var _someModule = require("someModule.js");
+
+(0, _someModule.myfunction)();
---
+++ www/someModule.js
@@ -0,0 +1,12 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.myfunction = void 0;
+
+const myfunction = () => {
+  console.log("I'm alive!");
+};
+
+exports.myfunction = myfunction;
