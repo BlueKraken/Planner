0d98cfa3bf570efec9f8dbc5d9e5396767ec5f7b
BlueKraken <gonzalo.tajmuch@gmail.com>
2018-11-01 15:59:09 -0300

    Agrega getters para presentation y totalScore del estado

--- src/califications.js
+++ src/califications.js
@@ -1,5 +1,7 @@
 class Calification {
-  constructor(weight = 0, score = 0) {
+  constructor( weight = 0, score = 0) {
+    // TODO: revertir el uso de key interno, corresponde a su índice dentro de
+    // la colección de calificaciones del estado y cambia por factores externos
     this.weight = weight;
     this.score = score;
     /** Calculates and return the weightedScore */
@@ -9,21 +11,27 @@ class Calification {
 
 const state = {
   califications: [
+    new Calification(),
     new Calification()
   ],
-  presentation: new Calification(),
   exam: new Calification(),
-  totalScore: 0
+  get presentation() {
+    const weight = 100 - this.exam.weight;
+    const score = this.califications
+    .map(c => c.weightedScore)
+    .reduce((total, current) => total += current);
+
+    return new Calification(weight, score);
+  },
+  get totalScore() { 
+    return this.presentation.weightedScore + this.exam.weightedScore
+  }
 }
 
 export const initCalificationsGrid = () => {
-  updatePresentationScore();
+  render(gridHeader, getCalificationGridContainerRef());
   renderCalifications();
-}
-
-export const onAddCalificationClick = () => {
-  const lenght = state.califications.push(new Calification());
-  renderCalificationRow(newCalification, lenght - 1);
+  renderTotals();
 }
 
 export const setExamScore = value => {
@@ -33,18 +41,16 @@ export const setExamScore = value => {
 
 export const setExamWeight = value => {
   state.exam.weight = value * 1;
-  state.presentation.weight = 100 - (value * 1);
   onExamChange();
 }
 
-const onExamChange = () => {  
-  updateTotalScore();
+export const onAddCalificationClick = () => {
+  state.califications.push(new Calification());
+
+  renderCalifications();
 }
 
-const updateTotalScore = () => {
-  state.totalScore = state.presentation.weightedScore
-    + state.exam.weightedScore;
-  
+const onExamChange = () => {  
   renderTotalScore();
 }
 
@@ -53,59 +59,33 @@ const renderTotalScore = () => {
   state.totalScore.toFixed(3);
 }
 
-const onCalificationsChange = () => {
-  updatePresentationScore();
-  updateTotalScore();
-}
-
-const updatePresentationScore = () => {
-  state.presentation.score = state.califications
-  .map(calification => calification.weightedScore)
-  .reduce((totalPoints, currentPoints) => totalPoints += currentPoints, 0);
-  
-  renderPresentationScore();
-}
-
-const renderPresentationScore = () => {
-  document.getElementById('presentation-score').innerText = 
-  state.presentation.score.toFixed(3);
-}
-
 const getCalificationGridContainerRef = () => 
-document.querySelector('#califications-container .grid-container');
+  document.querySelector('#califications-container .grid-container');
 
-const htmlCalificationRol = (calification, key) => {
-  // para usar efectivamente este patrón se debe implementar
-  // o funciones que manejen la calificación, o métodos de la misma.
-  // Puedo ver las calificaciones como objetos con comportamiento u
-  // estructuras de datos. Eso determinará la orientación de la programación
-  // (funcional u OO).
-  return `
-  <input class="grid-item" type="text" min="0" value="${calification}"
-  `
-}
+import { render, html } from '../node_modules/lit-html/lit-html.js';
 
-const renderCalificationRow = (calification, key) => {
+const renderCalificationRow = key => {
+  const calification = state.califications[key];
+
   const weightInputEl = document.createElement('input');
   weightInputEl.classList.add('grid-item');
   weightInputEl.setAttribute('type', 'number');
   weightInputEl.setAttribute('min', '0');  
   weightInputEl.setAttribute('value', calification.weight);
   weightInputEl.addEventListener('input', _ => {
-    state.califications[key].weight = 
-    weightInputEl.value * 1;
-    onCalificationsChange();
+    state.califications[key].weight = weightInputEl.value * 1;
+    renderTotals();
   });
   
   const scoreInputEl = document.createElement('input');
   scoreInputEl.classList.add('grid-item');
   scoreInputEl.setAttribute('type', 'number');
   scoreInputEl.setAttribute('min', '0');
+  scoreInputEl.setAttribute('step', '0.1');
   scoreInputEl.setAttribute('value', calification.score);
   scoreInputEl.addEventListener('input', _ => {
-    state.califications[key].score = 
-    scoreInputEl.value * 1;
-    onCalificationsChange();
+    state.califications[key].score = scoreInputEl.value * 1;
+    renderTotals();
   });
   
   const deleteButtonEl = document.createElement('button');
@@ -113,8 +93,8 @@ const renderCalificationRow = (calification, key) => {
   deleteButtonEl.textContent = 'Eliminar';
   deleteButtonEl.onclick = _ => {
     state.califications.splice(key, 1);
-    onCalificationsChange();
     renderCalifications();
+    renderTotals();
   }
   
   const containerRef = getCalificationGridContainerRef();
@@ -123,17 +103,30 @@ const renderCalificationRow = (calification, key) => {
   containerRef.appendChild(deleteButtonEl);
 }
 
-const renderCalifications = () => {
-  const containerRef = getCalificationGridContainerRef();
-  
-  containerRef.querySelectorAll(':not(.grid-header)').forEach(
-    element => element.remove());
-    
-    state.califications.forEach((calification, index) => {
-      renderCalificationRow(calification, index);
-    });
-    
-    renderTotalScore();
-  }
-  
-  
+const renderCalifications = () => {  
+  getCalificationGridContainerRef()
+  .querySelectorAll(':not(.grid-header)')
+  .forEach(e => e.remove());
+
+  state.califications.forEach((_c, i) => {
+    renderCalificationRow(i);
+  });
+}
+
+function renderTotals() {
+  renderPresentationScore();
+  renderTotalScore();
+}
+
+const renderPresentationScore = () => {
+  document.getElementById('presentation-score').innerText = 
+  state.presentation.score.toFixed(3);
+}
+
+const gridHeader = html`
+  <div class="grid-item grid-header">Peso</div>
+  <div class="grid-item grid-header">Nota</div>
+  <div class="grid-item grid-header">Opciones</div>
+`
+
+window.printState = () => console.log(state);
