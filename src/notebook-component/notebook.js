f535e1c3a7d0d74be447768f5ea87acbbe3d6f50
BlueKraken <gonzalo.tajmuch@gmail.com>
2018-11-01 23:15:35 -0300

    cambia objeto notebook a definición de clase

--- src/notebook-component/notebook.js
+++ src/notebook-component/notebook.js
@@ -1,12 +1,12 @@
 import { Calification } from './calification.js'
 
 
-export const notebook = {
-  califications: [
-    new Calification(50, 6),
-    new Calification(50, 4)
-  ],
-  exam: new Calification(),
+export class Notebook {
+  constructor() {
+    this.califications = [new Calification(100, 7)];
+    this.exam = new Calification();
+  }
+
   get presentation() {
     const weight = 100 - this.exam.weight;
     const score = this.califications
@@ -14,10 +14,9 @@ export const notebook = {
     .reduce((total, current) => total += current);
     
     return new Calification(weight, score);
-  },
+  }
+
   get totalScore() { 
     return this.presentation.weightedScore + this.exam.weightedScore
   }
 }
-
-window.printState = () => console.log(notebook);
