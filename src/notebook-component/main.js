4f37958ad030e0cfe6ba732070fc5db4ab66494a
BlueKraken <gonzalo.tajmuch@gmail.com>
2018-11-01 23:17:10 -0300

    arregla inicializacion de notebook

--- src/notebook-component/main.js
+++ src/notebook-component/main.js
@@ -1,3 +1,3 @@
-import { controller } from './viewModel.js';
+import { initNotebook } from './viewModel.js';
 
-document.addEventListener('DOMContentLoaded', _ => controller.init());
+document.addEventListener('DOMContentLoaded', _ => initNotebook());
